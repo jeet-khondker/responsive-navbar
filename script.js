db637d2612595f2311a9adcf80f35aaab25579d2
Jeet Khondker <jeetzhkhondker@gmail.com>
2023-11-19 17:56:23 +0900

    feature : Initialized Variables

---
+++ script.js
@@ -0,0 +1,5 @@
+const dropdownBtn = document.querySelectorAll(".dropdown-btn");
+const dropdown = document.querySelectorAll(".dropdown");
+const hamburgerBtn = document.getElementById("hamburger");
+const navMenu = document.querySelector(".menu");
+const links = document.querySelectorAll(".dropdown a");
